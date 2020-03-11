e97bf557934882306d8da2b69e1bc5e21c896950
Darksice <Darksice99@gmail.com>
2020-03-11 16:01:23 +0100

    init

---
+++ exo3_3/src/main/java/fr/uvsq/exo3_3/Robot.java
@@ -0,0 +1,12 @@
+package fr.uvsq.exo3_3;
+
+public class Robot {
+	private Position position;
+	private Direction direction;
+	
+	public Robot(Position pPosition, Direction pDirection)
+	{
+		this.position = pPosition;
+		this.direction = pDirection;
+	}
+}

e3e9a7e307c641713977891e84a695b06599e74a
root <root@localhost.localdomain>
2020-03-18 13:43:06 +0000

    ajout d'une class rebotStatic

---
+++ exo3_3/src/main/java/fr/uvsq/exo3_3/RobotStatic.java
@@ -0,0 +1,12 @@
+package fr.uvsq.exo3_3;
+
+public class RobotStatic {
+	private Position position;
+	private Direction direction;
+
+	public RobotStatic(Position pP,Direction pD)
+	{
+		this.position = pP;
+		this.direction = pD;
+	}
+}
